abba1df2d6aa6bfee52fbc5ff6f7717aebed6792
jaimis <jaimis@users.noreply.github.com>
2020-09-17 17:41:46 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/piccker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.piccker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'URL Launcher',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'URL Launcher'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future<void> _launched;
+  int _phone = 78985562323;
+
+  Future<void> _makePhoneCall(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(padding: const EdgeInsets.all(16.0), child: Text("")),
+              RaisedButton(
+                onPressed: () => setState(() {
+                  _launched = _makePhoneCall('tel:$_phone');
+                }),
+                child: const Text('Make phone call'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
